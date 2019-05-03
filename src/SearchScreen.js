b0b8e2edeb561803737023f572d2ae8f06b777e4
dinhgiang <dinhvangiang1198@gmail.com>
2019-05-03 14:15:57 +0700

    add viewCount, duration to search screen

--- src/SearchScreen.js
+++ src/SearchScreen.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
-import { ScrollView, Image, SafeAreaView } from 'react-native';
+import { ScrollView, Image, SafeAreaView, Text, View } from 'react-native';
 import { ListItem, SearchBar } from "react-native-elements";
 import axios from 'axios';
+import moment from 'moment';
 
+function numberFormatter(num, digits) {
+  var si = [
+    { value: 1, symbol: "" },
+    { value: 1E3, symbol: "K" },
+    { value: 1E6, symbol: "M" },
+    { value: 1E9, symbol: "B" }
+  ];
+  for (var i = si.length - 1; i > 0; i--) {
+    if (num >= si[i].value) {
+      break;
+    }
+  }
+  var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+}
 export default class SearchScreen extends Component {
   state = {
     search: '',
@@ -11,41 +27,44 @@ export default class SearchScreen extends Component {
     listItem: []
   };
 
-  onSearchSubmit(text) {
-    this.setState({ isLoading: true, search: text })
-    axios.get('https://www.googleapis.com/youtube/v3/search', {
+  getVideoDetails(videoId) {
+    return axios.get('https://www.googleapis.com/youtube/v3/videos', {
       params: {
-        part: "snippet",
-        q: text,
-        type: "video",
-        maxResults: 10,
+        part: "snippet,statistics,contentDetails",
+        id: videoId,
+        fields: 'items(id,snippet,statistics(viewCount),contentDetails(duration))',
         key: process.env.YOUTUBE_API_KEY
       }
     }).then((response) => {
-      this.setState({
-        listItem: response.data.items,
-        nextPageToken: response.data.nextPageToken,
-        isLoading: false
-      })
+      return response.data.items[0]
     }).catch((error) => {
       console.log(error);
     });
   }
 
-  onSearchNext(text, pageToken) {
+  onSearch(text, maxResults, pageToken) {
+    if (!pageToken) {
+      this.setState({ listItem: [] })
+    }
     this.setState({ isLoading: true, search: text })
     axios.get('https://www.googleapis.com/youtube/v3/search', {
       params: {
         part: "snippet",
         q: text,
         type: "video",
-        maxResults: 5,
+        maxResults: maxResults,
         pageToken: pageToken,
         key: process.env.YOUTUBE_API_KEY
       }
     }).then((response) => {
+      response.data.items.map(async item => {
+        const video = await this.getVideoDetails(item.id.videoId);
+        const duration = moment(moment.duration(video.contentDetails.duration)._data)
+        video.contentDetails.duration = duration.isBefore(1, 'h') ? duration.format("m:ss") : duration.format("H:mm:ss")
+        video.statistics.viewCount = numberFormatter(video.statistics.viewCount);
+        this.setState({ listItem: [...this.state.listItem, video] })
+      })
       this.setState({
-        listItem: [...this.state.listItem, ...response.data.items],
         nextPageToken: response.data.nextPageToken,
         isLoading: false
       })
@@ -73,14 +92,14 @@ export default class SearchScreen extends Component {
           showLoading={this.state.isLoading}
           value={this.state.searchInput}
           onChangeText={searchInput => this.setState({ searchInput })}
-          onSubmitEditing={event => this.onSearchSubmit(event.nativeEvent.text)}
+          onSubmitEditing={event => this.onSearch(event.nativeEvent.text, 7)}
         />
 
         <ScrollView
           style={{ paddingTop: 7 }}
           onScroll={({ nativeEvent }) => {
             if (this.isCloseToBottom(nativeEvent) && this.state.listItem.length < 50 && this.state.listItem.length != 0) {
-              this.onSearchNext(this.state.search, this.state.nextPageToken)
+              this.onSearch(this.state.search, 5, this.state.nextPageToken)
             }
           }}
           scrollEventThrottle={5000}
@@ -91,21 +110,29 @@ export default class SearchScreen extends Component {
                 key={key}
                 containerStyle={{ alignItems: 'flex-start', backgroundColor: null, paddingTop: 7, paddingBottom: 7 }}
                 leftElement={
-                  <Image
-                    style={{ width: item.snippet.thumbnails.medium.width / 2, height: item.snippet.thumbnails.medium.height / 2 }}
-                    source={{ uri: item.snippet.thumbnails.medium.url }}
-                  />
+                  <View style={{}}>
+                    <Image
+                      style={{ width: item.snippet.thumbnails.medium.width / 2, height: item.snippet.thumbnails.medium.height / 2 }}
+                      source={{ uri: item.snippet.thumbnails.medium.url }}
+                    />
+                    <Text style={{ position: 'absolute', bottom: 5, right: 5, backgroundColor: 'black', color: 'white', opacity: 0.7, padding: 2, borderRadius: 2, overflow: 'hidden', fontSize: 12 }}>
+                      {item.contentDetails.duration}
+                    </Text>
+                  </View>
                 }
                 title={item.snippet.title}
                 titleStyle={{ color: 'white' }}
                 titleProps={{ numberOfLines: 3 }}
-                subtitleStyle={{ color: 'black', fontSize: 12 }}
-                subtitle={item.snippet.channelTitle}
+                subtitle={
+                  <View>
+                    <Text>{item.snippet.channelTitle}</Text>
+                    <Text>{item.statistics.viewCount + ' views'}</Text>
+                  </View>
+                }
+                subtitleStyle={{ color: 'black', fontSize: 11 }}
                 subtitleProps={{ numberOfLines: 2 }}
                 pad={10}
                 onPress={() => this.props.navigation.navigate('Play', { videoId: item.id.videoId })}
-
-
               />
             )
           })}
